6a60c80a39702a848a0969a26f947c15bd13b96d
Daniel Dóniz García <danieldonizgarcia@gmail.com>
2023-01-07 00:45:27 +0000

    Initial Commit

---
+++ AnimalDEX/AnimalDEXApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalDEXApp.swift
+//  AnimalDEX
+//
+//  Created by Daniel Dóniz García on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimalDEXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
